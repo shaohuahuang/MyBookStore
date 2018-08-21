a89f7c5e8c36ad68050fc0572eadabc6e7fed48c
shaohua <shaohuahuang@dbs.com>
2018-08-21 14:48:51 +0800

    first commit

---
+++ bookstoreclient/src/main/java/com/example/bookstoreclient/BookstoreclientApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstoreclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreclientApplication.class, args);
+	}
+}
---
+++ bookstoreserver/src/main/java/com/example/bookstoreserver/BookstoreserverApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookstoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreserverApplication.class, args);
+	}
+}
